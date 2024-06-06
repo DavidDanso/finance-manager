de23c17a5ca2150341282f856e60746ee10e0d7d
DavidDanso <desiingerboy@icloud.com>
2024-06-06 00:03:14 +0000

    add user profile for editing

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('profile', views.profilePage, name='profile'),
+    path('logout', views.logoutPage, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
 from .forms import *
-from django.contrib.auth import get_user_model
 
 
 # login view
@@ -26,6 +23,42 @@ def login_view(request):
         else:
             messages.error(request, 'Invalid username or password')
     return render(request, 'accounts/login.html')
+
+
+########################################## profile page views
+@login_required(login_url='login')
+def profilePage(request):
+    # user profile
+    profile = get_object_or_404(Profile, user=request.user)
+
+    # user profile form INSTANCE
+    form = UserProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        # user profile form
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,  'Profile updated Successful✅')
+            if request.user.role == 'admin':
+                return redirect('dashboard')
+            elif request.user.role == 'accountant':
+                return redirect('reports')
+
+    
+        elif 'delete_account' in request.POST:
+                profile.delete()
+                messages.success(request, 'Account delete Successful')
+                return redirect('login')
+
+    context = {'form': form, 'profile': profile}
+    return render(request, 'profile.html', context)
+
+
+################################ logout views
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
 
 
 
